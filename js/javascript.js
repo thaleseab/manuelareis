6af7b54d2faffd54efd7a04286d04522b21fa19d
Thales Everardo Albuquerque Reis <thales.eareis@gmail.com>
2024-03-08 18:47:11 -0500

    new verswion

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,22 @@
 function share(url, title, text) {
-    if (navigator.share) {
-      navigator.share({
-        title: title,
-        text: text,
-        url: url,
-      })
-      .then(() => console.log('Sharing successful'))
-      .catch((error) => console.log('Error sharing:', error));
-    } else {
-      alert('Sharing API is not supported in your device or browser.');
-    }
+  if (navigator.share) {
+    navigator.share({
+      title: title,
+      text: text,
+      url: url,
+    })
+    .then(() => console.log('Sharing successful'))
+    .catch((error) => console.log('Error sharing:', error));
+  } else {
+    alert('Sharing API is not supported in your device or browser.');
   }
+}
+
+function scrollToElement(elementId, offset) {
+  var element = document.getElementById(elementId);
+  var offsetPosition = element.offsetTop - offset;
+  window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth"
+  });
+}
